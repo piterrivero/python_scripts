f9c606e453e0a94bc3851c9db87ca4fce7c060ef
Piter Rivero <Piter Rivero>
2019-02-01 13:43:56 +0100

    complete version

--- servicesTypeMigration.py
+++ servicesTypeMigration.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import redis
 
+node1 = 'devbe01.svil.actalis.it'
+node2 = 'devbe02.svil.actalis.it'
+node3 = 'devbe03.svil.actalis.it'
+
 try:
     conn = redis.StrictRedis(
-    host='devbe01.svil.actalis.it',
+    host=node2,
     port=16379,
     password='$redis0123!')
 #    print (conn)
@@ -21,11 +25,16 @@ for service in serviceList:
 	idType = conn.hget('mwconfig:service:'+service.decode('utf-8'),'idType')
 	if idType is None:
 		type = conn.hget('mwconfig:service:'+service.decode('utf-8'),'type')
+		type = type.decode('utf-8')
 		serviceTypeList = conn.hgetall('mwconfig:serviceTypes')
-		for serviceType in serviceTypeList:
-			print (serviceType)
+		for key, value in serviceTypeList.items():
+			if type == value.decode('utf-8') :
+				key_name = 'mwconfig:service:'+service.decode('utf-8')
+				idTypeValue = key.decode('utf-8')
+				conn.hset(key_name, 'idType', idTypeValue)
+				
 	
-	
+
 
 
 	
